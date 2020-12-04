6ba8ba6db6d64fd03eaa8c55cdb65f29cab1cb32
Cristina <cnlopez8442@gmail.com>
2020-12-04 15:25:35 +0100

    comentarios

--- clientes_sin_codest/routes/client.routes.js
+++ clientes_sin_codest/routes/client.routes.js
@@ -3,13 +3,13 @@ module.exports = app => {
   
     var router = require("express").Router();
   
-    // Retrieve all clients
+    // Extraer todos los clientes
     router.get("/", clients.findAll);
   
-    // Retrieve a single client with id
+    // Extraer un cliente segun id
     router.get("/:id", clients.findOne);
   
-    // Update a client with id
+    // Actualizar un cliente segun id
     router.put("/:id", clients.update);
   
     app.use('/clients', router);
