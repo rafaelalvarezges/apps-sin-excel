a8b5b0553e0bd4dc02e23765910f38eead859cf6
Cristina <cnlopez8442@gmail.com>
2020-12-23 17:05:36 +0100

    añadimos nav

--- menu-apps/src/App.js
+++ menu-apps/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import {List} from "./components/List/List";
+import Nav from './components/nav/Nav';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav></Nav>
       <List></List>
     </div>
   );
---
+++ menu-apps/src/components/nav/Nav.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import styles from './Nav.module.scss';
+import { INavProps } from './INavProps';
+
+export default class Nav extends React.Component<INavProps>{
+    constructor(props: INavProps) {
+        super(props);
+        this.state = {
+            state: ""
+        }
+    }
+
+    public render(): React.ReactElement<INavProps> {
+        return (
+            <div className={styles.Nav}>
+                <nav className="navbar navbar-light">
+                    <a className="navbar-brand" href="#">
+                    <img src="./logo.png" width="200px" className="d-inline-block align-top" alt=""/>
+                    </a>
+                    <div className={styles.title}>
+                            {this.props.title}
+                            </div>
+                    </nav>
+            </div>
+        );
+    }
+}
