6785fd82c3d2e7ab425499724c2f3634f11a2d39
Cristina <cnlopez8442@gmail.com>
2021-01-20 11:32:41 +0100

    añadimos codigos estandard

--- clientes_sin_codest/src/services/export.js
+++ clientes_sin_codest/src/services/export.js
@@ -6,13 +6,43 @@ import {
 
 
 export default class Export extends React.Component {
+    codigos = ["ADMON. PUBLICA", "BAJA DEFINITIVA", "CENTRO COMERCIAL", "CLIENTE DEL GRUPO", "CLIENTES CONTADOS", "COMPAÑIA ELECTRICA", "COMPETIDOR"]
+    lastItem = 0
+    datos = {}
     constructor(props){
         super(props);
+        this.datos = props.data
     }
     _exporter;
     export = () => {
+        for(let i = 0; i < this.codigos.length; i++){
+            console.log(this.datos[i])
+            if(this.datos[i] != undefined){
+                let d = {...this.datos[i], "codigos": this.codigos[i]}
+                this.datos[i] = d
+            }else{
+                console.log("es undefined")
+                let d = this.datos
+                d.push({
+                    cadena: null,
+                    codcli: "",
+                    codest: null,
+                    global: "",
+                    nombre: "",
+                    nomdrv: "",
+                    nomext: "",
+                    pdv: "",
+                    vta_anio_actual: "",
+                    vta_anio_ant: "",
+                    _id: "",
+                    "codigos": this.codigos[i]
+                })
+                this.datos = d
+
+            }
+        }
         this._exporter.save(this.props.data);
-        console.log(this.props.data)
+        // console.log(this.props.data)
         this.props.setDownload(false)
     }
     
@@ -21,13 +51,16 @@ export default class Export extends React.Component {
         return (
             <div>
                 <ExcelExport
-                    data={this.props.data}
+                    data={this.datos}
                     fileName="Listado.xlsx"
                     ref={(exporter) => { this._exporter = exporter; if(exporter)this.export() }}
                 >
                      {this.props.cols.map((col, index) =>{
+                         this.lastItem = index
                          return <ExcelExportColumn field={col.dataField} title={col.text} key={index} />
                     })}
+
+                    <ExcelExportColumn field="codigos" title="Códigos" key={this.lastItem + 1} />
                     
                 </ExcelExport> 
             </div>
