552f05a4116445457c8db0b5741972929347fef6
rafaelalvarezges <rafael.alvarez@grupoelectrostocks.com>
2021-01-27 09:52:31 +0000

    Actualización registros mongo

--- clientes_sin_codest/pythons/clientes_sin_codest.py
+++ clientes_sin_codest/pythons/clientes_sin_codest.py
@@ -6,7 +6,7 @@ def carga_datos():
     col = 'clientes_sin_codest'
     db = conectar_mongo()
     collect = db[col]
-    collect.drop()
+    collect.delete_many({})
     i = 1
 
     dict_mongo = {'_id':0, 
