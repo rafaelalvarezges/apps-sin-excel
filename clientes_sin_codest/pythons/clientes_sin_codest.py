fec193408e64c0532de749661895147b60e66f5c
Cristina <cnlopez8442@gmail.com>
2021-03-30 09:49:40 +0200

    cambio en la select de clientes sin codest

--- clientes_sin_codest/pythons/clientes_sin_codest.py
+++ clientes_sin_codest/pythons/clientes_sin_codest.py
@@ -22,9 +22,44 @@ def carga_datos():
                     'vta_anio_actual':0.0
                 }
 
+    # str_select = '''
+    #     Select pdv, codcli, numidc, nombre, cadena, codest, nomext, nomdrv, venta_anio_anterior, venta_anio_actual
+    #         from v_clientes_sin_codest
+    # '''
     str_select = '''
-        Select pdv, codcli, numidc, nombre, cadena, codest, nomext, nomdrv, venta_anio_anterior, venta_anio_actual
-            from v_clientes_sin_codest
+        select * from 
+        (select  rac.pdv pdv, 
+                cli.codcli, 
+                rac.idc numidc, 
+                rac.nomcli nombre,
+                rac.cad cadena,
+                cli.codest codest, 
+                rac.nomext ce,
+                rac.nomdrv drv,
+                sum(case when to_char(to_date(sls.anymes,'YYYYMM'),'YYYY') = to_char(add_months(sysdate,-13),'yyyy') then sls.imp_vta else 0 end) Venta_ant,
+                sum(case when to_char(to_date(sls.anymes,'YYYYMM'),'YYYY') = to_char(add_months(sysdate,-1),'yyyy') then sls.imp_vta else 0 end) Venta_act
+        from RAC_COL_PRF_C@DWH rac, 
+            clientes cli,  
+            v_resumen_asignaciones@dwh ra,   
+            sf_sls_50@DWH sls
+        where rac.idc = cli.numidc
+            and (
+                (substr(rac.cliente,5) = to_char(cli.codcli))
+                or
+                (instr(rac.cliente,'.',5) > 0 and  substr(rac.cliente,5,instr(rac.cliente,'.',5)-5) = to_char(cli.codcli) ) --algunos clientes tienen concatenado al final .30 (sucursal)
+                )
+            and rac.pdv = cli.codemp
+            and ra.codemp(+) = cli.codemp
+            and ra.codcli(+) = cli.codcli
+            and fsociedad(rac.pdv) != '852'
+            and rac.pdv not in ('851','200','199','012')
+            and estado != 0
+            and cli.codest is null
+            and cli.codemp = sls.codemp
+            and cli.codcli = sls.codcli
+        group by rac.pdv,cli.codcli,rac.idc, rac.nomcli, rac.cad, cli.codest, rac.nomext, rac.nomdrv
+        order by pdv, codcli
+        ) where nvl(venta_ant,0)+nvl(venta_act,0)>0;
     '''
 
     _, cur = conectar_oracle()
